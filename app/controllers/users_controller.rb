d6fc317d0f52184c9a4fa864152dbb254d18bcd0
John <jpena91@gmail.com>
2019-04-17 12:30:55 -0400

    creat login controller, add validations, add route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    
+  end
+
+
+
+
+  private
+
+  def user_params(*args)
+    params.require(:user).permit(*args)
+  end
+
+
 end
