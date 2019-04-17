1685321bcab5e9f99d9e99ffef82e3450c16f0f1
John <jpena91@gmail.com>
2019-04-17 17:22:21 -0400

    fixed logout

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,6 @@ Rails.application.routes.draw do
   root to: "application#index"
 
   resources :users, only: [:index, :show, :new, :create, :edit, :update, :destroy]
-
-  # get 'login', to: 'login#new'
-  post 'login', to: 'login#create'
-  #
-  # delete '/logout', to: 'login#destroy'
   resources :logins, only: [:new, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
