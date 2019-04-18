2bb850e6a29811c60623ef00bed54368749aef39
Jeff Yu <jwy5140@gmail.com>
2019-04-18 18:17:45 -0400

    Switched to postgres

--- db/migrate/20190416210350_create_media.rb
+++ db/migrate/20190416210350_create_media.rb
@@ -2,8 +2,11 @@ class CreateMedia < ActiveRecord::Migration[5.2]
   def change
     create_table :media do |t|
       t.string :title
-      t.string :genre
-      t.integer :runtime
+      t.string :genres
+      t.string :runtime
+      t.string :release_year
+      t.string :rating
+      t.string :rating_votes
     end
   end
 end
